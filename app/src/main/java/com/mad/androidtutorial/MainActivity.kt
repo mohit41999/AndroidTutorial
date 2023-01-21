cd8e0aee6c2e81812e0e0e74dfa1b7ee4265c2f2
mohit <mohit41999@gmail.com>
2023-01-21 01:28:34 -0500

    comment message

--- app/src/main/java/com/mad/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/mad/androidtutorial/MainActivity.kt
@@ -2,7 +2,7 @@ package com.mad.androidtutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//this is a comment in branch 1
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
